255fac9581aa2f12a2400618b6cb8da894985d75
Veno <joshwad178@outlook.com>
2023-11-01 13:23:29 -0500

    fix

--- app/views.py
+++ app/views.py
@@ -4,27 +4,27 @@ from django.http.request import HttpRequest
 import random
 
 
-def hello_view(request: HttpRequest, name):
-    return HttpResponse(f'Hello {name}')
+def hello_view(request: HttpRequest, name:str) -> str:
+    return HttpResponse(f'Hello, {name}!')
 
 
-def roll_die_view(request: HttpRequest, sides):
+def roll_die_view(request: HttpRequest, sides:int) -> int:
     roll = random.randint(1, sides)
     return HttpResponse(roll)
 
 
-def random_between_view(request, lo, hi):
+def random_between_view(request:HttpRequest, lo:int, hi:int) -> int:
     return HttpResponse(random.randint(lo, hi))
 
-def add_view(request, num1, num2):
+def add_view(request:HttpRequest, num1:int, num2:int) -> int:
     return HttpResponse(num1 + num2)
 
-def hey_view(request, name):
+def hey_view(request:HttpRequest, name:str) -> str:
     return HttpResponse(f'Hey, {name}!')
 
-def age_in_view(request, Year, BirthYear):
+def age_in_view(request:HttpRequest, Year:int, BirthYear:int) -> int:
     return HttpResponse(Year - BirthYear)
 
-def order_total_view(request, burg, fry, drinks):
+def order_total_view(request:HttpRequest, burg:int, fry:int, drinks:int) -> int:
     return HttpResponse((burg*4.5)+(fry*1.5)+drinks)
 
